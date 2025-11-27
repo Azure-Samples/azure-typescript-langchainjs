d726682a287c8bb7a682a00c26f7042c6d905f87
Dina Berry (MSFT) <diberry@microsoft.com>
2025-11-27 00:02:13 +0000

    token

--- packages/langgraph-agent/src/azure/azure-credential.ts
+++ packages/langgraph-agent/src/azure/azure-credential.ts
@@ -1,16 +1,9 @@
-import { DefaultAzureCredential } from "@azure/identity";
+import { DefaultAzureCredential, getBearerTokenProvider } from "@azure/identity";
 
 export const CREDENTIAL = new DefaultAzureCredential();
 
 export const SCOPE_OPENAI = "https://cognitiveservices.azure.com/.default";
 export const SCOPE_SEARCH = "https://search.azure.com/.default";
 
-export async function azureADTokenProvider_OpenAI() {
-  const tokenResponse = await CREDENTIAL.getToken(SCOPE_OPENAI);
-  return tokenResponse.token;
-}
-
-export async function azureADTokenProvider_Search() {
-  const tokenResponse = await CREDENTIAL.getToken(SCOPE_SEARCH);
-  return tokenResponse.token;
-}
+export const azureADTokenProvider_OpenAI = getBearerTokenProvider(CREDENTIAL, SCOPE_OPENAI);
+export const azureADTokenProvider_Search = getBearerTokenProvider(CREDENTIAL, SCOPE_SEARCH);
